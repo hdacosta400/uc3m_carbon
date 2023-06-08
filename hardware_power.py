f6a65e88a4962c40d4e156ecdcdd93a6a09ba928
Howard DaCosta <howard.dacosta400@gmail.com>
2023-06-08 11:24:59 -0400

    included code carbon repo

---
+++ hardware_power.py
@@ -0,0 +1,8 @@
+'''
+Detects the CPU hardware being used, determines power usage
+
+
+intel power gadget seems to handle this for intel machines? --> total power, not just for CPU
+
+multiply detected TDP (w) x time elapsed (H) x 1000 for kwH??
+'''
