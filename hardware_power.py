92947fbea1533a4a5efaa9392180641c1b172aa5
Howard DaCosta <howard.dacosta400@gmail.com>
2023-06-14 14:35:25 +0200

    added codecarbon repo, test script for code carbon + parsing txt files, initial Mx feature changes

--- hardware_power.py
+++ hardware_power.py
@@ -6,3 +6,34 @@ intel power gadget seems to handle this for intel machines? --> total power, not
 
 multiply detected TDP (w) x time elapsed (H) x 1000 for kwH??
 '''
+
+import sys 
+sys.path.append("codecarbon")
+from codecarbon import EmissionsTracker
+
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10),
+    ]
+)
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
+
+tracker = EmissionsTracker()
+tracker.start()
+model.fit(x_train, y_train, epochs=10)
+emissions: float = tracker.stop()
+print(emissions)
